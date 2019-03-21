6caabded6ab4a23eac683f54dc69ae2ce09d7641
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-03-21 14:15:11 +0100

    All methods now fit the new database structure :ok_hand:

--- src/main/java/dal/UserDAOImpls185136.java
+++ src/main/java/dal/UserDAOImpls185136.java
@@ -17,14 +17,26 @@ public class UserDAOImpls185136 implements IUserDAO {
     @Override
     public IUserDTO getUser(int userId) throws DALException{
         try (Connection c = createConnection()){
+            IUserDTO user = new UserDTO();
             PreparedStatement getUsr = c.prepareStatement(
-                    "SELECT * FROM Afl1Users WHERE userID = ?");
+                    "SELECT * FROM Afl2_Users WHERE user_id = ?");
             getUsr.setInt(1, userId);
-            ResultSet resSet = getUsr.executeQuery();
-            IUserDTO user = null;
-            if (resSet.next()){
-                user = makeUserFromResultSet(resSet);
+            ResultSet userResSet = getUsr.executeQuery();
+
+            PreparedStatement getUsrRols = c.prepareStatement(
+                    "SELECT role_name FROM Afl2_Roles INNER JOIN Afl2_UserRoles ON role_id = roles_id WHERE users_id = ?");
+            getUsrRols.setInt(1, userId);
+            ResultSet roleResSet = getUsrRols.executeQuery();
+
+            if (userResSet.next()){
+                user.setUserId(userResSet.getInt("user_id"));
+                user.setUserName(userResSet.getString("user_username"));
+                user.setIni(userResSet.getString("user_ini"));
             }
+            if (roleResSet.next()){
+                user.addRole(roleResSet.getString("role_name"));
+            }
+
             return user;
         } catch (SQLException e){
             throw new DALException(e.getMessage());
@@ -36,12 +48,12 @@ public class UserDAOImpls185136 implements IUserDAO {
 
         try (Connection c = createConnection()){
             PreparedStatement getUsrLst = c.prepareStatement(
-                    "SELECT * FROM Afl1Users");
+                    "SELECT user_id FROM Afl2_Users");
             ResultSet resSet = getUsrLst.executeQuery();
             List<IUserDTO> users = new ArrayList<>();
 
             while (resSet.next()){
-                IUserDTO user = makeUserFromResultSet(resSet);
+                IUserDTO user = getUser(resSet.getInt("user_id"));
                 users.add(user);
             }
             return users;
@@ -54,35 +66,58 @@ public class UserDAOImpls185136 implements IUserDAO {
     public void createUser(IUserDTO user) throws DALException{
         try (Connection c = createConnection()){
             PreparedStatement createUser = c.prepareStatement(
-                    "INSERT INTO Afl1Users (userID, userName, ini, roles) VALUES (?, ?, ?, ?)");
-            String rolesStr = String.join(";", user.getRoles());
+                    "INSERT INTO Afl2_Users (user_id, user_username, user_ini) VALUES (?, ?, ?)");
             createUser.setInt(1, user.getUserId());
             createUser.setString(2, user.getUserName());
             createUser.setString(3, user.getIni());
-            createUser.setString(3, rolesStr);
-
             createUser.executeUpdate();
+
+            PreparedStatement delOldRoles = c.prepareStatement(
+                    "DELETE FROM Afl2_UserRoles WHERE users_id = ?");
+            delOldRoles.setInt(1, user.getUserId());
+            delOldRoles.executeUpdate();
+
+            for (String s : user.getRoles()){
+                PreparedStatement role = c.prepareStatement(
+                        "SELECT role_id FROM Afl2_Roles WHERE role_name = ? LIMIT 1;");
+                role.setString(1, s);
+                ResultSet roleSet = role.executeQuery();
+                if (roleSet.next()){
+                    assignRole(user, roleSet, c);
+                } else {
+                    PreparedStatement newRole = c.prepareStatement(
+                            "INSERT INTO Afl2_Roles (role_name) VALUES (?)");
+                    newRole.setString(1, s);
+                    newRole.executeUpdate();
+
+                    PreparedStatement getRoleId = c.prepareStatement("SELECT role_id FROM Afl2_Roles WHERE role_name = ?");
+                    getRoleId.setString(1, s);
+                    ResultSet roleId = getRoleId.executeQuery();
+                    if (roleId.next()){
+                        assignRole(user, roleId, c);
+                    }
+                }
+            }
 
         } catch (SQLException e){
             throw new DALException(e.getMessage());
         }
     }
 
+    private void assignRole(IUserDTO user, ResultSet resSet, Connection c) throws SQLException{
+        PreparedStatement assignRole = c.prepareStatement(
+                "INSERT INTO Afl2_UserRoles (users_id, roles_id) VALUES (?, ?)");
+        assignRole.setInt(1, user.getUserId());
+        assignRole.setInt(2, resSet.getInt(1));
+        assignRole.executeUpdate();
+    }
+
+
     @Override
     public void updateUser(IUserDTO user) throws DALException{
         try (Connection c = createConnection()){
-            PreparedStatement updUser = c.prepareStatement(
-                    "UPDATE Afl1Users SET " +
-                            "userName = ?, " +
-                            "ini = ?, " +
-                            "roles = ? " +
-                            "WHERE userID = ?");
-            String rolesStr = String.join(";", user.getRoles());
-            updUser.setString(1, user.getUserName());
-            updUser.setString(2, user.getIni());
-            updUser.setString(3, rolesStr);
-            updUser.setInt(4, user.getUserId());
-            updUser.executeUpdate();
+            deleteUser(user.getUserId());
+            createUser(user);
 
         } catch (SQLException e){
             throw new DALException(e.getMessage());
@@ -92,26 +127,17 @@ public class UserDAOImpls185136 implements IUserDAO {
     @Override
     public void deleteUser(int userId) throws DALException{
         try (Connection c = createConnection()){
+            PreparedStatement delUsrRols = c.prepareStatement(
+                    "DELETE FROM Afl2_UserRoles WHERE users_id = ?");
             PreparedStatement delUsr = c.prepareStatement(
-                    "DELETE FROM Afl1Users WHERE userId = ? ");
+                    "DELETE FROM Afl2_Users WHERE user_id = ? ");
+            delUsrRols.setInt(1, userId);
             delUsr.setInt(1, userId);
+            delUsrRols.executeUpdate();
             delUsr.executeUpdate();
 
         } catch (SQLException e){
             throw new DALException(e.getMessage());
         }
     }
-
-    private IUserDTO makeUserFromResultSet(ResultSet resSet) throws SQLException{
-        IUserDTO user = new UserDTO();
-        user.setUserId(resSet.getInt("userID"));
-        user.setUserName(resSet.getString("userName"));
-        user.setIni(resSet.getString("ini"));
-        String rolesStr = resSet.getString("roles");
-        String[] rolesArr = rolesStr.split(";");
-        List<String> rolesList = Arrays.asList(rolesArr);
-        user.setRoles(rolesList);
-        return user;
-    }
-
 }
